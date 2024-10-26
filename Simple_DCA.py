0be58368b021874ebf7c02f5b979e2e293763b5b
975 Pjotr <pjotr975@gmail.com>
2024-10-26 21:04:18 +0200

    apró módosítások, változók neveinek optimalizálása

--- Simple_DCA.py
+++ Simple_DCA.py
@@ -13,8 +13,8 @@ pjotr957@gmail.com
 import yfinance as yf
 
 # A vizsgált instrumentum tickerje és a vizsgált periódus
-ticker = "tsla"
-period = "max"  # Period a következő értékek valamelyike lehet ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+ticker = "mcd"
+period = "1y"  # Period a következő értékek valamelyike lehet ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
 # meghatározott időintervallum adatainak líehívása: ticker.history(start="2015-01-01", end="2020-12-31")
 
 # A vállalat nevének lekérdezése és kiíratása a ticker alapján
@@ -30,20 +30,21 @@ print(f"A vizsgált időszak: {period}\n-------------------")
 capital = 10000
 comission_min = 1
 comission = 0.001
+base_order_ASAP = True
 initial_drop_percent = 5
 drop_increment_multiplier = 1
 safety_order_NR = 5
 base_quant = 1
 safety_quant = 1
-safety_quant_multiplier = 1
+safety_quant_multiplier = 1 # kizárólag egész szám lehet!
 
 # egyéb globális változók definiálása
-buyAndHold_quantity = 0
-remain_cash = 0
-startdate = 0
-startclose = 0
-highCapital = 0
-maxdrawdown = 0
+BH_quantity = 0
+BH_remain_cash = 0
+BH_startdate = 0
+BH_startclose = 0
+BH_highCapital = 0
+BH_maxdrawdown = 0
 
 # Adatok letöltése a Yahoo Finance-ről (OHLC, date és volume)
 data = yf.download(ticker, period=period)  # Az elmúlt {period} adatait töltjük le
@@ -60,31 +61,31 @@ for i in range(len(lows)):
 
     # buy and hold referenciához a packett méretének és a maradék cash-nek kiszámítása
     if i == 0:
-        startclose = closes[i]
-        startdate = dates[i].strftime("%Y-%m-%d")
-        buyAndHold_quantity = capital // startclose
-        remain_cash = capital - capital * comission - buyAndHold_quantity * startclose
+        BH_startclose = closes[i]
+        BH_startdate = dates[i].strftime("%Y-%m-%d")
+        BH_quantity = capital // BH_startclose
+        BH_remain_cash = capital - capital * comission - BH_quantity * BH_startclose
 
         # ha a jutalék miatt mínuszba futna a maradék cash, itt módosítjuk levefé az eszköz darabszámot, amíg pozitív maradékunk lesz
-        while remain_cash < 0:
-            buyAndHold_quantity -= 1
-            remain_cash = capital - capital * comission - buyAndHold_quantity * startclose
-        print(f"Buy and hold stratégia kiindulási adatai.\nStart: {startdate} | Close price: {startclose:.2f} | Shares: {buyAndHold_quantity} | remain cash: {remain_cash:.2f}")
+        while BH_remain_cash < 0:
+            BH_quantity -= 1
+            BH_remain_cash = capital - capital * comission - BH_quantity * BH_startclose
+        print(f"Buy and hold stratégia kiindulási adatai.\nStart: {BH_startdate} | Close price: {BH_startclose:.2f} | Shares: {BH_quantity} | remain cash: {BH_remain_cash:.2f}")
 
     # drawdown számítása buy and hold alatt
-    actualcapital = closes[i] * buyAndHold_quantity + remain_cash
-    if actualcapital > highCapital:
-        highCapital = actualcapital
+    BH_actualcapital = closes[i] * BH_quantity + BH_remain_cash
+    if BH_actualcapital > BH_highCapital:
+        BH_highCapital = BH_actualcapital
     else:
-        drawdown = (highCapital - actualcapital) / highCapital * 100
-        if drawdown > maxdrawdown:
-            maxdrawdown = drawdown
+        BH_drawdown = (BH_highCapital - BH_actualcapital) / BH_highCapital * 100
+        if BH_drawdown > BH_maxdrawdown:
+            BH_maxdrawdown = BH_drawdown
 
     # buy and hold stratégia zárása
     if i == (len(lows) - 1):
         close = closes[i]
         date = dates[i].strftime("%Y-%m-%d")
-        close_capital = remain_cash + buyAndHold_quantity * close - buyAndHold_quantity * close * comission
-        buyAndHold_profit = close_capital - capital
-        buyAndHold_profit_percent = (buyAndHold_profit / capital) * 100
-        print(f"Buy and hold stratégia eredménye.\nStart: {startdate} | Close price: {startclose:.2f} | Shares: {buyAndHold_quantity:.0f} | remain cash: {remain_cash:.2f}\nEnd:   {date} | Close price: {close:.2f} | Capital: {close_capital:.2f} | Profit: {buyAndHold_profit:.2f} | Profit %: {buyAndHold_profit_percent:.2f} | Max. drawdown: {maxdrawdown:.2f}%")
+        BH_close_capital = BH_remain_cash + BH_quantity * close - BH_quantity * close * comission
+        BH_profit = BH_close_capital - capital
+        BH_profit_percent = (BH_profit / capital) * 100
+        print(f"Buy and hold stratégia eredménye.\nStart: {BH_startdate} | Close price: {BH_startclose:.2f} | Shares: {BH_quantity:.0f} | remain cash: {BH_remain_cash:.2f}\nEnd:   {date} | Close price: {close:.2f} | Capital: {BH_close_capital:.2f} | Profit: {BH_profit:.2f} | Profit %: {BH_profit_percent:.2f} | Max. drawdown: {BH_maxdrawdown:.2f}%")
